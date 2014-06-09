d2ea62e08341e13a6bddac88e688651311068f22
Vignesh Sivashanmugam <vignesh.siva@gmail.com>
2014-06-09 17:02:28 -0400

    Removed update methods from objects

---
+++ core/src/com/kilobolt/balldrop/gameobjects/Brick.java
@@ -0,0 +1,25 @@
+package com.kilobolt.balldrop.gameobjects;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Brick extends GameObject{
+
+	public Brick(float x, float y, float width, float height, float velX,
+			float velY, int drawOrder) {
+		super(x, y, width, height, velX, velY, drawOrder);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void draw(SpriteBatch batch) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateGameObject() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
